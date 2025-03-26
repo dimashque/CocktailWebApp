baf815cd98f1610b23853f686f56309a4fae9c95
dimashque <dimashk89@hotmail.com>
2025-03-26 17:28:04 +0100

    Added Login And Register

---
+++ CocktailWebApp/Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+﻿using CocktailWebApp.Data;
+using CocktailWebApp.ViewModel;
+using CocktailWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CocktailWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            //append Claim to logged in User
+         /*   var profilePictureUrl = user.ProfileImageUrl;
+            await _userManager.AddClaimAsync(user, new Claim("profile_picture", profilePictureUrl)); 
+         */
+
+            if (user != null)
+            {
+                // user is found check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    // password correct signIn
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+
+                }
+                // password Wrong!!
+                TempData["Error"] = "Wrong Credential, try again ";
+                return View(loginVM);
+            }
+            //User Not found !!
+            TempData["Error"] = "Wrong Credential, try again ";
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Register(RegisterViewModel RegisterVM)
+        {
+            if (!ModelState.IsValid) return View(RegisterVM);
+
+            var user = await _userManager.FindByEmailAsync(RegisterVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This User already exists";
+                return View(RegisterVM);
+            }
+
+            var newUser = new AppUser()
+            {
+                Email = RegisterVM.EmailAddress,
+                UserName = RegisterVM.UserName,
+
+
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, RegisterVM.Password);
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return RedirectToAction("Index", "Event");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CocktailWebApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CocktailWebApp.ViewModel
+{
+    public class LoginViewModel
+    {
+        
+            [Display(Name = "Email Address")]
+            [DataType(DataType.EmailAddress)]
+            [Required]
+            public string EmailAddress { get; set; }
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+       
+    }
+}
