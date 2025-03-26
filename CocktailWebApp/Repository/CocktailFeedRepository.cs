faac93ea24d91c1435037ecf59ae9e64cc6be260
dimashque <dimashk89@hotmail.com>
2025-03-26 18:32:27 +0100

    Added CocktailFeed

---
+++ CocktailWebApp/Repository/CocktailFeedRepository.cs
@@ -0,0 +1,21 @@
+﻿using CocktailWebApp.Data;
+using CocktailWebApp.Interfaces;
+using CocktailWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CocktailWebApp.Repository
+{
+    public class CocktailFeedRepository : ICocktailFeedRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CocktailFeedRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<AppUser>> GetAllUsersAsync()
+        {
+            return await _context.Users.Include( e => e.CreatedCocktails).ToListAsync();
+        }
+    }
+}
